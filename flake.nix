7c942d84d83b94b38d80c97dc6be3bad41d4b19e
Kevin Joseph <kevinjoseph1995@gmail.com>
2025-11-01 11:15:10 -0700

    First commit with basic setup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs system; };
+        modules = [ ./configuration.nix ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,100 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+  # ===== USER & HOME =====
+  # Home Manager needs a bit of information about you and the paths it should manage.
+  home.username = "kevin";
+  home.homeDirectory = "/home/kevin";
+
+  # ===== HOME MANAGER VERSION =====
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes. You should not change this value
+  # without reading the release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  # ===== PACKAGES =====
+  # Install user-scoped packages here.
+  home.packages = [
+    # pkgs.hello
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # ===== FILES (DOTFILES) =====
+  # Manage plain files via home.file.
+  home.file = {
+    # ".screenrc".source = dotfiles/screenrc;
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # ===== ENVIRONMENT VARIABLES =====
+  # If you don't manage your shell with Home Manager, source hm-session-vars.sh manually.
+  home.sessionVariables = {
+    EDITOR = "hx";
+  };
+
+  # ===== PROGRAMS =====
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    # Settings written as TOML. Add your preferences here.
+    settings = {
+      # Key mappings equivalent to Helix TOML:
+      # [keys.normal."space"]
+      # o = "file_picker_in_current_buffer_directory"
+      keys = {
+        normal = {
+          "space" = {
+            o = "file_picker_in_current_buffer_directory";
+          };
+        };
+      };
+      theme = "gruvbox";
+    };
+    # Language-specific configuration.
+    languages = {
+      language = [
+        {
+          name = "nix";
+          auto-format = true;
+          formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+        }
+      ];
+    };
+  };
+
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      gs = "git status";
+      gpl = "git pull";
+      grep = "rg";
+    };
+  };
+
+  programs.fzf.enable = true;
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Kevin Joseph";
+        email = "kevinjoseph1995@gmail.com";
+      };
+    };
+  };
+}
